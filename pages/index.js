6faaf0959e209b3ab6436952b9492d00d108ab73
Juan Pablo Giron <juan.giron@ihme3d.com>
2020-09-10 10:28:29 -0500

    Testing changes

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Sketch from "react-p5";
-
-
-const TestP5 = (props) => {
-  let x = 50;
-  const y = 50;
-
-  const setup = (p5, canvasParentRef) => {
-      p5.createCanvas(500, 500).parent(canvasParentRef);
-  };
-
-  const draw = (p5) => {
-      p5.background(0);
-      p5.ellipse(x, y, 70, 70);
-      x++;
-  };
-
-  return <Sketch setup={setup} draw={draw} />;
-}
 
 export default function Home() {
 
   return (
     <div className={styles.container}>
-      <h1> Test </h1>
-      <TestP5 />
+      <h1> Test 2 </h1>
     </div>
   )
 }
