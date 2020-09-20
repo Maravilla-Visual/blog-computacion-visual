f244f731981da128235f7570a4898a3e8a66a87d
Juan Pablo Giron <juan.giron@ihme3d.com>
2020-09-19 19:15:39 -0500

    Added error page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/ErrorMessage.css'
 import Navbar from '../components/navbar/navbar'
 
 function MyApp({ Component, pageProps }) {
---
+++ pages/_error.js
@@ -0,0 +1,19 @@
+
+
+const Error = () => {
+  return (
+    <div id="container">
+      <div class="content">
+        <h2 className="error-message">404</h2>
+        <h4 className="error-title">Página no encontrada <span>😢</span></h4>
+        <p className="error-description">
+          La página que buscabas no se encuentra. Puede que hayas escrito mal la
+          dirección o se haya removido.
+        </p>
+        <a className="back-home" href="/blog-computacion-visual">Devuelta al Home</a>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
