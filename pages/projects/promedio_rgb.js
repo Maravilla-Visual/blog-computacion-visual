a2249d0ac1495bcbf5a71a5cd1fe42695990d85d
Juan Pablo Giron <juan.giron@ihme3d.com>
2020-10-04 13:56:07 -0500

    Added whit background

--- pages/projects/promedio_rgb.js
+++ pages/projects/promedio_rgb.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import { Row, Col } from "react-bootstrap";
 
@@ -13,57 +13,60 @@ const PromedioRGB = () => {
 
   const handleChange = (event) => {
     setFile(URL.createObjectURL(event.target.files[0]));
+    setShowSketch(false);
   };
 
   const handleConvertClick = () => {
-    const newShowSketchValue = !showSketch;
-    setShowSketch(newShowSketchValue);
-  }
+    setShowSketch(true);
+  };
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Row>
-        <Col xs={2}>
-          <input
-            type="file"
-            onChange={handleChange}
-            placeholder="holi"
-            style={{ marginTop: "1rem" }}
-          />
-        </Col>
-        <Col xs={4}>
-          <img
-            src={file}
-            style={{ width: "200px", height: "200px" }}
-          />
-        </Col>
-        <Col xs={2}>
-          {file && (
-            <button
-              className="back-home"
-              style={{ outline: "none", border: "none" }}
-              onClick={handleConvertClick}
-            >
-              Convertir
-            </button>
-          )}
-        </Col>
-        <Col xs={4}>
-          {file && showSketch && (
-            <div>
-              <PromedioRGBSketch imageURL={file} />
-            </div>
-          )}
-        </Col>
-      </Row>
+    <div style={{ height: "100%", padding: '5rem' }}>
+      <div className="projects-container" style={{ height: "100%", width: '100%' }} />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          position: 'absolute',
+          top: 0,
+          left: '6rem',
+        }}
+      >
+        <Row>
+          <Col xs={2}>
+            <input
+              type="file"
+              onChange={handleChange}
+              placeholder="holi"
+              style={{ marginTop: "1rem" }}
+            />
+          </Col>
+          <Col xs={4}>
+            <img src={file} style={{ width: "200px", height: "200px" }} />
+          </Col>
+          <Col xs={2}>
+            {file && (
+              <button
+                className="back-home"
+                style={{ outline: "none", border: "none" }}
+                onClick={handleConvertClick}
+              >
+                Convertir
+              </button>
+            )}
+          </Col>
+          <Col xs={4}>
+            {file && showSketch && (
+              <div>
+                <PromedioRGBSketch imageURL={file} />
+              </div>
+            )}
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
